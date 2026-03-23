c661df24a00aecf20ee4e60a6497384ac84c384f
siltdo9-wq <siltdo9@gmail.com>
2026-03-23 09:46:56 +0100

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,1 +1,49 @@
+import Stripe from "stripe";
 
+const stripe =
+new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req,res){
+
+const { amount } = req.body;
+
+const session =
+await stripe.checkout.sessions.create({
+
+payment_method_types: ["card"],
+
+line_items:[{
+
+price_data:{
+
+currency:"eur",
+
+product_data:{
+name:"Acompte SDO Web"
+},
+
+unit_amount: amount * 100,
+
+},
+
+quantity:1,
+
+}],
+
+mode:"payment",
+
+success_url:
+"https://sdo-web.be/client-space.html",
+
+cancel_url:
+"https://sdo-web.be/devis/devis.html",
+
+});
+
+res.json({
+
+url: session.url
+
+});
+
+}
